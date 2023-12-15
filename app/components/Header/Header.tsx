3bfb11981a0ffed19850bfe0750b602352fc2116
cirmialexis <1320538+cirmialexis@users.noreply.github.com>
2023-12-15 04:00:48 -0300

    Working on header

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -12,13 +12,9 @@ export const Header = () => {
     <header className={`bg-transparent flex justify-center items-center ${zen.className} ${styles.header}`}>
       <nav>
         <div id='title' className='flex antialiased'>
-          <h1 className='text-5xl'>A</h1>
-          <h1 className='text-5xl -scale-x-100'>N</h1>
-          <h1 className='text-5xl'>V</h1>
-          <h1 className='text-5xl'>E</h1>
-          <h1 className='text-5xl'>R</h1>
-          <h1 className='text-5xl'>S</h1>
-          <h1 className='text-5xl'>O</h1>
+          <span className='text-5xl'>A</span>
+          <span className='text-5xl -scale-x-100'>N</span>
+          <span className='text-5xl'>VERSO</span>
         </div>
       </nav>
     </header>

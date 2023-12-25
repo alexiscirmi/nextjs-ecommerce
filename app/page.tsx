5d15a7a419feb7e4fd56e7bc9dfccabe7c2888c1
cirmialexis <1320538+cirmialexis@users.noreply.github.com>
2023-12-25 10:44:51 -0300

    Working on main page

--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,26 @@ export default function Home() {
           style={{ height: '80vh' }}
         />
       </section>
+      <section className='md:grid md:grid-cols-2'>
+        <div className='p-4'>
+          <Image
+            src={main1}
+            alt=''
+            width='4050'
+            height='2700'
+            className='object-cover object-top'
+          />
+        </div>
+        <div className='p-4'>
+          <Image
+            src={main1}
+            alt=''
+            width='4050'
+            height='2700'
+            className='object-cover object-top'
+          />
+        </div>
+      </section>
     </main>
   )
 }

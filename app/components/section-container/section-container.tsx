33446862cf203677209863a0669b3d3a39986e4e
alexiscirmi <1320538+alexiscirmi@users.noreply.github.com>
2024-03-23 02:30:11 -0300

    UI tweaks

--- app/components/section-container/section-container.tsx
+++ app/components/section-container/section-container.tsx
@@ -5,7 +5,7 @@ export const SectionContainer = ({
 }) => {
   return (
     <section
-      className='flex flex-wrap justify-evenly py-3 md:pt-10 pb-20 px-10'
+      className='flex flex-wrap justify-evenly py-3 md:pt-10 pb-20 px-10 lg:px-28 xl:px-40'
       style={{ minHeight: '88vh' }}
     >
       {children}
